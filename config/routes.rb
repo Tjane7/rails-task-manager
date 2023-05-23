16c7fe5ec7edb343ec07e9780fdca7304062a376
tainah jane <tainah.94@hotmail.com>
2023-05-23 16:58:25 -0400

    tasks

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,11 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  get '/tasks', to: 'tasks#index'
+  # create
+  get '/tasks/new', to: 'tasks#new', as: :new_task
+  post '/tasks', to: 'tasks#create'
+  # show one
+  get '/tasks/:id', to: 'tasks#show', as: :task
+
 end
